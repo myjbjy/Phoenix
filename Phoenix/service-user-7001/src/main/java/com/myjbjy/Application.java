1cf8d530192b12d171ad74be88a8c4294a809275
myj <18817671776@163.com>
2022-12-26 16:11:56 +0800

    添加用户服务配置文件，几个基础服务的pom文件修改

--- Phoenix/service-user-7001/src/main/java/com/myjbjy/Application.java
+++ Phoenix/service-user-7001/src/main/java/com/myjbjy/Application.java
@@ -1,17 +1,13 @@
 package com.myjbjy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * @author myj
- */
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class
-})
-@EnableDiscoveryClient  // 开启注册中心的服务注册和发现功能
+@SpringBootApplication
+@EnableDiscoveryClient  // 开启服务的注册与发现功能
+@MapperScan(basePackages = "com.myjbjy.mapper")
 public class Application {
 
     public static void main(String[] args) {
