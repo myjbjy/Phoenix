74a1f0aaf6edef0bdb5a3a300aa219e7cc247644
myj <18817671776@163.com>
2023-01-15 13:14:00 +0800

    Gateway过滤器校验JWT-header组装用户

--- Phoenix/gateway-8000/src/main/java/com/myjbjy/filter/SecurityFilterJWT.java
+++ Phoenix/gateway-8000/src/main/java/com/myjbjy/filter/SecurityFilterJWT.java
@@ -1,6 +1,7 @@
 package com.myjbjy.filter;
 
 import com.google.gson.Gson;
+import com.myjbjy.base.BaseInfoProperties;
 import com.myjbjy.grace.result.GraceJSONResult;
 import com.myjbjy.grace.result.ResponseStatusEnum;
 import com.myjbjy.utils.JWTUtils;
@@ -14,6 +15,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
@@ -30,7 +32,7 @@ import java.util.List;
  */
 @Component
 @Slf4j
-public class SecurityFilterJWT implements GlobalFilter, Ordered {
+public class SecurityFilterJWT extends BaseInfoProperties implements GlobalFilter, Ordered {
 
     public static final String HEADER_USER_TOKEN = "headerUserToken";
 
@@ -77,7 +79,16 @@ public class SecurityFilterJWT implements GlobalFilter, Ordered {
             String prefix = tokenArr[0];
             String jwt = tokenArr[1];
 
-            return dealJWT(jwt, exchange, chain);
+            // 判断并且处理用户信息
+            if (prefix.equalsIgnoreCase(TOKEN_USER_PREFIX)) {
+                return dealJWT(jwt, exchange, chain, APP_USER_JSON);
+            } else if (prefix.equalsIgnoreCase(TOKEN_SAAS_PREFIX)) {
+                return dealJWT(jwt, exchange, chain, SAAS_USER_JSON);
+            } else if (prefix.equalsIgnoreCase(TOKEN_ADMIN_PREFIX)) {
+                return dealJWT(jwt, exchange, chain, ADMIN_USER_JSON);
+            }
+
+//            return dealJWT(jwt, exchange, APP_USER_JSON, chain);
         }
 
         // 到达此处表示被拦截
@@ -89,10 +100,10 @@ public class SecurityFilterJWT implements GlobalFilter, Ordered {
         return renderErrorMsg(exchange,ResponseStatusEnum.UN_LOGIN);
     }
 
-    public Mono<Void> dealJWT(String jwt, ServerWebExchange exchange, GatewayFilterChain chain){
+    public Mono<Void> dealJWT(String jwt, ServerWebExchange exchange, GatewayFilterChain chain, String headerKey){
         try {
-            String userJson = jwtUtils.checkJWT(jwt);
-            return chain.filter(exchange);
+            // 重构header，将用户数据往后传递
+            return chain.filter(setNewHeader(exchange,headerKey,jwtUtils.checkJWT(jwt)));
         } catch (ExpiredJwtException e) {
             e.printStackTrace();
             return renderErrorMsg(exchange,ResponseStatusEnum.JWT_EXPIRE_ERROR);
@@ -100,6 +111,19 @@ public class SecurityFilterJWT implements GlobalFilter, Ordered {
             e.printStackTrace();
             return renderErrorMsg(exchange,ResponseStatusEnum.JWT_SIGNATURE_ERROR);
         }
+    }
+
+    public ServerWebExchange setNewHeader(ServerWebExchange exchange,
+                                          String headerKey,
+                                          String headerValue){
+        // 重新构建新的request
+        ServerHttpRequest newRequest = exchange.getRequest()
+                .mutate()
+                .header(headerKey, headerValue)
+                .build();
+
+        // 替换原来的request
+        return exchange.mutate().request(newRequest).build();
     }
 
     /**
