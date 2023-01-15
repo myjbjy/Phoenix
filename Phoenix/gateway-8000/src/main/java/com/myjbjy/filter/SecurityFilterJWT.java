ba0fbc53f0f888e454065052c5146bca6ae6941f
myj <18817671776@163.com>
2023-01-15 10:19:02 +0800

    Gateway过滤器校验JWT-包装自定义错误

--- Phoenix/gateway-8000/src/main/java/com/myjbjy/filter/SecurityFilterJWT.java
+++ Phoenix/gateway-8000/src/main/java/com/myjbjy/filter/SecurityFilterJWT.java
@@ -1,17 +1,28 @@
 package com.myjbjy.filter;
 
+import com.google.gson.Gson;
+import com.myjbjy.grace.result.GraceJSONResult;
+import com.myjbjy.grace.result.ResponseStatusEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+/**
+ * @author myj
+ */
 @Component
 @Slf4j
 public class SecurityFilterJWT implements GlobalFilter, Ordered {
@@ -46,8 +57,35 @@ public class SecurityFilterJWT implements GlobalFilter, Ordered {
         // 到达此处表示被拦截
         log.info("被拦截了。。。");
 
-        // 放行
-        return chain.filter(exchange);
+        // 不放行，token校验在jwt校验的自身代码逻辑中，到达此处表示都是漏掉的可能没有配置在excludeList
+//        GraceException.display(ResponseStatusEnum.UN_LOGIN);
+//        return chain.filter(exchange);
+        return renderErrorMsg(exchange,ResponseStatusEnum.UN_LOGIN);
+    }
+
+    /**
+     * 重新包装并且返回错误信息
+     * @param exchange
+     * @param statusEnum
+     * @return
+     */
+    public Mono<Void> renderErrorMsg(ServerWebExchange exchange,
+                                     ResponseStatusEnum statusEnum) {
+        // 1. 获得response
+        ServerHttpResponse response = exchange.getResponse();
+        // 2. 构建jsonResult
+        GraceJSONResult jsonResult = GraceJSONResult.exception(statusEnum);
+        // 3. 修改response的code为500
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+        // 4. 设定header类型
+        if (!response.getHeaders().containsKey("Content-Type")) {
+            response.getHeaders().add("Content-Type", MimeTypeUtils.APPLICATION_JSON_VALUE);
+        }
+        // 5. 转换json并且向response中写入数据
+        String resultJson = new Gson().toJson(jsonResult);
+        DataBuffer dataBuffer = response.bufferFactory().wrap(resultJson.getBytes(StandardCharsets.UTF_8));
+
+        return response.writeWith(Mono.just(dataBuffer));
     }
 
     // 过滤器的顺序，数字越小优先级则越大
